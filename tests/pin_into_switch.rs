3036f99ee776d231bed557c671f365e33e113891
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2020-02-13 21:55:10 -0500

    Consolidate  functions

--- tests/pin_into_switch.rs
+++ tests/pin_into_switch.rs
@@ -1,16 +1,16 @@
-
 use switch_hal::mock::{Pin, State};
+use switch_hal::IntoSwitch;
 
 mod output_pin {
     use super::*;
 
     use embedded_hal::digital::v2::InputPin;
-    use switch_hal::{OutputSwitch, OutputPinExt};
+    use switch_hal::OutputSwitch;
 
     #[test]
-    fn into_active_high() {
+    fn active_high() {
         let pin = Pin::new();
-        let mut switch = pin.into_active_high_output();
+        let mut switch = pin.into_active_high_switch();
         switch.on().unwrap();
 
         let pin = switch.into_pin();
@@ -18,9 +18,9 @@ mod output_pin {
     }
 
     #[test]
-    fn into_active_low() {
+    fn active_low() {
         let pin = Pin::new();
-        let mut switch = pin.into_active_low_output();
+        let mut switch = pin.into_active_low_switch();
         switch.on().unwrap();
 
         let pin = switch.into_pin();
@@ -30,19 +30,19 @@ mod output_pin {
 
 mod input_pin {
     use super::*;
-    use switch_hal::{InputSwitch, InputPinExt};
+    use switch_hal::InputSwitch;
 
     #[test]
-    fn into_active_high() {
+    fn active_high() {
         let pin = Pin::with_state(State::High);
-        let switch = pin.into_active_high_input();
+        let switch = pin.into_active_high_switch();
         assert_eq!(true, switch.is_active().unwrap());
     }
 
     #[test]
-    fn into_active_low() {
+    fn active_low() {
         let pin = Pin::with_state(State::Low);
-        let switch = pin.into_active_low_input();
+        let switch = pin.into_active_low_switch();
         assert_eq!(true, switch.is_active().unwrap());
     }
 }
